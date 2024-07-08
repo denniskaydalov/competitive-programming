b44e5c449288c722cafd1fa363ebeca6c387fd04
denniskaydalov <denniskaydalov@gmail.com>
2024-07-07 22:52:40 -0400

    Add CF Round 956 A

---
+++ cf/956/a.py
@@ -0,0 +1,7 @@
+for _ in range(int(input())):
+    n = int(input())
+    # n, m = list(map(int, input().split()))
+    # a = list(map(int, input().split()))
+    # s = input()
+    print(*list(range(1, n + 1)))
+    
